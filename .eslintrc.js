7b9a3544f04563d6488b3fe33f88e4226851d37b
MistaPidaus <firdaus.kamal251@gmail.com>
2021-03-22 11:14:26 +0800

    Setup ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:cypress/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    curly: 'error',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/display-name': 'warn',
+    'react/prop-types': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.test.tsx'],
+      rules: {
+        // Allow testing runtime errors to suppress TS errors
+        '@typescript-eslint/ban-ts-comment': 'off',
+      },
+    },
+  ],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
