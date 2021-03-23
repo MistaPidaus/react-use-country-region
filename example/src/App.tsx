4a2c6955368d6c6b0cbe4891d5c7dfc2161fcb50
MistaPidaus <firdaus.kamal251@gmail.com>
2021-03-23 12:25:54 +0800

    docs: ✏️ update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,15 +5,42 @@ import { useCountryRegion, CountryRegionProp } from 'react-use-country-region';
 const App = () => {
   const [country, setCountry] = useState<CountryRegionProp>(null);
   const { result } = useCountryRegion(country);
+
+  document.title = 'React - useCountryRegion hook example';
+
   return (
     <div>
-      <button onClick={() => setCountry('MY')}>Country: Malaysia (MY)</button>
-      <button onClick={() => setCountry('US')}>Country: United States (US)</button>
-      <button onClick={() => setCountry('CA')}>Country: Canada (CA)</button>
-      <button onClick={() => setCountry('GB')}>Country: United Kingdom (GB)</button>
-      <button onClick={() => setCountry('JP')}>Country: Japan (JP)</button>
-      <button onClick={() => setCountry('CH')}>Country: Switzerland (CH)</button>
-      <pre>{JSON.stringify(result, null, 4)}</pre>
+      <div style={{ maxWidth: '1280px', margin: '0 auto', marginTop: '3rem' }}>
+        <h1>React - useCountryRegion hook example</h1>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(6, minmax(0, 1fr))',
+            gap: '1rem',
+            marginTop: '2rem',
+          }}
+        >
+          <button style={{ padding: '1rem' }} onClick={() => setCountry('MY')}>
+            Country: Malaysia (MY)
+          </button>
+          <button style={{ padding: '1rem' }} onClick={() => setCountry('US')}>
+            Country: United States (US)
+          </button>
+          <button style={{ padding: '1rem' }} onClick={() => setCountry('CA')}>
+            Country: Canada (CA)
+          </button>
+          <button style={{ padding: '1rem' }} onClick={() => setCountry('GB')}>
+            Country: United Kingdom (GB)
+          </button>
+          <button style={{ padding: '1rem' }} onClick={() => setCountry('JP')}>
+            Country: Japan (JP)
+          </button>
+          <button style={{ padding: '1rem' }} onClick={() => setCountry('CH')}>
+            Country: Switzerland (CH)
+          </button>
+          <pre>{JSON.stringify(result, null, 4)}</pre>
+        </div>
+      </div>
     </div>
   );
 };
