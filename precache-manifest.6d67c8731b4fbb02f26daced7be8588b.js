053b8d421bc2678861b9c4ad1457ab396a0fad33
MistaPidaus <firdaus.kamal251@gmail.com>
2021-03-23 12:37:19 +0800

    Updates

---
+++ precache-manifest.6d67c8731b4fbb02f26daced7be8588b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f668739c8202267924a0e47890da6d",
+    "url": "/react-use-country-region/index.html"
+  },
+  {
+    "revision": "f4fc4ecc4e4e1e0adc7b",
+    "url": "/react-use-country-region/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "f0d4933c52f6da479202",
+    "url": "/react-use-country-region/static/js/2.1d170705.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-use-country-region/static/js/2.1d170705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fc4ecc4e4e1e0adc7b",
+    "url": "/react-use-country-region/static/js/main.5bed7d21.chunk.js"
+  },
+  {
+    "revision": "4dac4cb369e037e1043a",
+    "url": "/react-use-country-region/static/js/runtime-main.deb41cf3.js"
+  }
+]);
